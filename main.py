3525be3fae7c1a93f266300939dcf3ff39f6842d
vdhanush02009-star <vdhanush02009@gmail.com>
2026-07-19 16:13:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_groq import ChatGroq
+import shutil
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "RAG Chatbot is running"}
+
+@app.post("/upload")
+def upload_pdf(file: UploadFile = File(...)):
+    file_path = f"uploads/{file.filename}"
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    loader = PyPDFLoader(file_path)
+    pages = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    chunks = splitter.split_documents(pages)
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma_db")
+    return {"message": f"{file.filename} processed - {len(chunks)} chunks stored"}
+
+@app.get("/ask")
+def ask_question(question: str):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+    llm = ChatGroq(model="llama-3.3-70b-versatile", api_key="YOUR_GROQ_API_KEY_HERE")
+    retriever = db.as_retriever()
+    docs = retriever.invoke(question)
+    context = "\n\n".join([doc.page_content for doc in docs])
+    response = llm.invoke(f"Answer based on context:\n\nContext: {context}\n\nQuestion: {question}")
+    return {"answer": response.content}
