730d5b82789e5ee2181e9239ffa21076bc65f7c8
vdhanush02009-star <vdhanush02009@gmail.com>
2026-07-20 09:51:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
-
 from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 import shutil
@@ -33,15 +32,15 @@ def upload_pdf(file: UploadFile = File(...)):
     pages = loader.load()
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     chunks = splitter.split_documents(pages)
-    embeddings = HuggingFaceEmbeddings(model_name="paraphrase-MiniLM-L3-v2")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
     db = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma_db")
     return {"message": f"{file.filename} processed - {len(chunks)} chunks stored"}
 
 @app.get("/ask")
 def ask_question(question: str):
-    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
     db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-    llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.geten("GROQ_API_KEY"))
+    llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=os.getenv("GROQ_API_KEY"))
     retriever = db.as_retriever()
     docs = retriever.invoke(question)
     context = "\n\n".join([doc.page_content for doc in docs])
